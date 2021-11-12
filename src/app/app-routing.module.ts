7e03fe3134d3c87e5bccbbc5f9212dfd32de600a
Anastas456 <wild456west@gmail.com>
2021-11-12 21:53:51 +0300

    страница со списком организаций

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrganizationsComponent } from './organizations/organizations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: OrganizationsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
